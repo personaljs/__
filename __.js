a6161c180bd41e10cb7a30cd957825789b096823
onmoon <ivan@on-moon.ru>
2014-06-27 22:19:28 +0400

    Update __.js

--- __.js
+++ __.js
@@ -1,47 +1,37 @@
-var __ = function (val, count) {
-	__.locale = 'en';
-	__.gender = 1;
-	return __.fn.get(val, count);
-};
-__.fn = __.prototype = {
-	locale : function(val) {
-		__.locale = val;
-	},
-	gender : function(val) { //0 - all, men , 1 - women
-		__.gender = val;
-	},
-	get : function(val, count) {
-		var word = locale[__.locale][val];
-		if (word) word = __.fn.replaceGender(word);
-		if (word && count) word = __.fn.replaceTime(word, count);
-		return word || val;
-	},
-	replaceGender: function(word) {
-		var w = word.match(/\[(.*?)\]/);//[0] - то что надо заменить [1] - чем заменить
-		if(!w) return word;
-		var res = w[1].split('|');
-		return word.replace(w[0], res[__.gender]);
-	},
-	replaceTime: function(word, count) {
-		var w = word.match(/\(([^\(\)]+)\)/);//[0] - то что надо заменить [1] - чем заменить
-		if(!w) return word;
-		var res = w[1].split('|');
-		return word.replace( w[0] ,res[  __.fn['count' + __.locale](count) ] );
-	},
-	countru: function(count) {
-		if (count > 10 && Math.floor((count % 100) / 10) == 1) {
-            return 2;
-        } else {
-        	switch (count % 10) {
-				case 1: return 0;
-				case 2:
-				case 3:
-				case 4: return 1;
-				default: return 2;
-			}
-		}
-	},
-	counten: function(count) {
-		return count === 1 ? 0 : 1; 
+function locale (obj) {
+	var lang = 'ru';
+	var gender = 0;
+	function replace (text, num) {
+		text = text.replace(/\[(.*)\]/gi, function(m,value){
+			var arr = value.split('|');
+			return arr[gender] || '';
+		});
+		text = text.replace(/\((.*)\)/gi, function(m,value){
+			var arr = value.split('|');
+			return arr[num] || '';
+		});
+		return text;
 	}
+	function getNumber (num) {
+		if(lang === 'en' && num > 1) return 1;
+		var val = num % 100;
+		if (val > 10 && val < 20) return 2;
+		else {
+			val = num % 10;
+			if (val === 1) return 0;
+			else if (val > 1 && val < 5) return 1;
+			else return 2;
+		}
+	}
+	function out (val, num) {
+		return replace(obj[lang][val], getNumber(num));
+	}
+	out.lang = function (val) {
+		lang = val;
+	}
+	out.gender = function (val) {
+		gender = val;
+	}
+	return out;
 };
+var __ = locale(locale);
